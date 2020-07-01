06d89d1444a7134c988ab7e59a19d83bd27420f5
vida85 <46607821+vida85@users.noreply.github.com>
2020-07-01 15:26:50 -0700

    Add files via upload

--- Probably_knot v3/main.py
+++ Probably_knot v3/main.py
@@ -3,51 +3,97 @@ from kivymd.theming import ThemeManager
 from kivymd.uix.dialog import MDDialog
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.list import TwoLineListItem
-from kivymd.uix.button import MDIconButton
-from kivymd import *
+from kivymd.uix.button import MDIconButton, MDFlatButton
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.chip import MDChip
+from kivy.clock import Clock
+from kivy.modules import keybinding
+from kivy.core.window import Window
 
 from PyDictionary import PyDictionary
 import sys
 import json
 import requests
+from functools import partial
 
+LIST = []
+
+MESSAGE = """
+Think of Probably Knot (PK) as a study
+guide of sorts. Helping the user guide
+himself.
+
+This little program was designed to help
+re-think ones sentences and therefore 
+find new solutions. By changing ones
+perception, things can become more clear
+where once they were misunderstood.
+
+PK re-shuffle a word from an input
+sentence to help rephrase ones 
+orignal sentence. To better
+understand problems and ideas by
+changing the angle of perception
+with more elegant solutions.
+"""
 
 class Manager(ScreenManager):
-    def change_screen(self, inst):
-        Manager.current = f'{inst.ids.main.text}'
+    pass
 
 class Main(Screen):
+    def on_pre_enter(self):
+        Window.bind(on_key_down=self.call)
+
     """main application goes here"""
+    # def on_touch_down(self, touch):
+    #     print("touched screen")
+    #     return super().on_touch_down(touch)
+
+    def navigation_draw(self):
+        print("navigation")
+
     def close_dialog(self, obj):
         self.dialog.dismiss()
 
-    def show_data(self):
-        message = """
-        Think of Probably Knot (PK) as a study
-        guide of sorts. Helping the user guide
-        himself.
-
-        This little program was designed to help
-        re-think ones sentences and therefore 
-        find new solutions. By changing ones
-        perception, things can become more clear
-        where once they were misunderstood.
-
-        PK re-shuffle a word from an input
-        sentence to help rephrase ones 
-        orignal sentence. To better
-        understand problems and ideas by
-        changing the angle of perception
-        with more elegant solutions.
-        """
+    def open_dialog(self):
         close = MDIconButton(icon="close-circle", on_release=self.close_dialog)
         #more = MDIconButton(icon="more")
-        self.dialog = MDDialog(title="Probably Knot Helper", text=message,
-                         size_hint=(0.8, 1), buttons=[close])
+        self.dialog = MDDialog(title="Probably Knot Helper",
+                               text=MESSAGE,
+                               size_hint=(0.8, 1), buttons=[close],)
         self.dialog.open()
+
+    def call(self, *args):
+        self.ids.stack.clear_widgets()
+        sent = self.ids.sentence.text.lower()
+        for word in sent.split():
+            c = MDChip(label=word,
+                       callback=self.do_something,
+                       icon='checkbox-blank-outline',
+                       color=[.1, .1, .1, .5],
+                       )
+            self.ids.stack.add_widget(c)
+        LIST = []
+
+    def do_something(self, inst, word, *args):
+        theme_cls = ThemeManager()
+        if inst.icon == 'checkbox-marked-outline':
+            inst.icon = 'checkbox-blank-outline'
+            inst.color = [.1, .1, .1, .5]
+        else:
+            inst.icon = 'checkbox-marked-outline'
+            inst.color = theme_cls.primary_color
+
+        LIST.append(word)
+        return LIST
 
 
 class Analyzer(Screen):
+    def on_touch_move(self, touch):
+        # print(touch)
+        with self.canvas:
+            print(dir(touch.ud))
+
     def on_leave(self):
         # print(dir(self.ids.container))
         self.ids.container.clear_widgets()
@@ -57,8 +103,7 @@ class Analyzer(Screen):
         """Analyse data with PyDictionary"""
 
         sent = main.ids.sentence.text.lower()
-        wrd = main.ids.word.text.lower()
-        print(sent, wrd)
+        wrd = LIST[0].lower()
 
         # Definition Section #
         dictionary = PyDictionary()
@@ -69,13 +114,15 @@ class Analyzer(Screen):
             URL = f'http://words.bighugelabs.com/api/2/{API_KEY}/{wrd}/json'
 
             if wrd not in sent:
-                print("i made it")
                 error = MDDialog(title="Error", text=f"Word: '{wrd}' is not in\n\n'{sent}'")
                 error.open()
             else:
                 r = requests.get(URL) # get's url json file
-                j = json.loads(r.text) # loads json into 'j' as a dict
-
+                try:
+                    j = json.loads(r.text) # loads json into 'j' as a dict
+                    print(len(j['noun']['syn']), j)
+                except:
+                    j = {''}
                 if type(j) == dict: # check is 'j' variable is coming in as a Dict holds the new sentences new = f"{result}\n"
                     final_set = set()
                     try:
@@ -99,15 +146,32 @@ class Analyzer(Screen):
                         item = TwoLineListItem(text=f"{sent.replace(wrd, word)}", secondary_text=f"{word}")
                         self.ids.container.add_widget(item)
 
+    def empty(self, *args):
+        self.dialog_one = MDDialog(
+            text="First screen dialog",
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL", text_color=self.theme_cls.primary_color),
+                MDFlatButton(
+                    text="DISCARD", text_color=self.theme_cls.primary_color)])
+        self.dialog_one.open()
+
 
 class ProbablyKnotApp(MDApp):
     def build(self):
         self.theme_cls = ThemeManager()
         self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Amber"
-        self.theme_cls.primary_hue = "A700"
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_hue = "900"
+
+        self.theme_cls.accent_palette = 'Blue'
+        self.theme_cls.accent_hue = "900"
 
         return Manager()
+
+    def change_screen(self, screen):
+        self.root.current = screen
+
 
 if __name__ == "__main__":
     ProbablyKnotApp().run()
